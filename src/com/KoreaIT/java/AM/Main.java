3d5ef55bf92b99b7da517a19f0698415a7915039
wkdwjdgns0920 <wkdwjdgns0920@gmail.com>
2023-03-08 16:38:39 +0900

    명령어 입력

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("==프로그램 시작==");
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-		String command = sc.nextLine();
-		System.out.printf("입력된 명령어 : %s\n",command);
-		
-		int command2 = sc.nextInt();
-		System.out.printf("입력된 명령어 : %d\n",command2);
-		
+
+		while (true) {
+			System.out.print("명령어 > ");
+			String command = sc.nextLine();
+			
+			if(command.equals("exit")) {
+				break;
+			}
+		}
+
 		System.out.println("==프로그램 끝==");
 
 		sc.close();
 	}
-
 }
