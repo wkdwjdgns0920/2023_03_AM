d8570de256bf083586d6ce88d8a21d0c4d1ba640
wkdwjdgns0920 <wkdwjdgns0920@gmail.com>
2023-03-13 13:21:39 +0900

    시험 1번

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -1,75 +1,26 @@
 package com.KoreaIT.java.AM;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
+
 	public static void main(String[] args) {
-		System.out.println("==프로그램 시작==");
 
-		Scanner sc = new Scanner(System.in);
+		for (int i = 1; i <= 3; i++) {
+			
+			for (int j = 1; j <= 3; j++) {
+				System.out.print("*");
 
-		int lastArticleId = 0;
-		List<Article> articles = new ArrayList<>();
-
-		while (true) {
-
-			System.out.print("명령어 > ");
-			String command = sc.nextLine();
-
-			if (command.length() == 0) {
-				System.out.println("명령어를 입력해주세요");
-				continue;
 			}
-
-			if (command.equals("exit")) {
-				break;
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		for (int i = 1; i <= 2; i++) {
+			for(int j = 1; j <= 4; j++) {
+				System.out.print("*");
 			}
-
-			if (command.equals("article list")) {
-				if (articles.size() == 0) {
-					System.out.println("게시글이 없습니다");
-				} else {
-					System.out.println(" 번호  //  제목  ");
-					for (int i = articles.size() - 1; i >= 0; i--) {
-						Article article = articles.get(i);
-						System.out.printf("  %d   //   %s  \n", article.id, article.title);
-					}
-				}
-
-			} else if (command.equals("article write")) {
-				int id = lastArticleId + 1;
-				System.out.print("제목 : ");
-				String title = sc.nextLine();
-				System.out.print("내용 : ");
-				String body = sc.nextLine();
-
-				Article article = new Article(id, title, body);
-				articles.add(article);
-
-				System.out.printf("%d번글이 생성되었습니다\n", id);
-				lastArticleId++;
-
-			} else {
-				System.out.println("존재하지 않는 명령어입니다");
-			}
+			System.out.println();
 		}
 
-		System.out.println("==프로그램 끝==");
-
-		sc.close();
-	}
-}
-
-class Article {
-	int id;
-	String title;
-	String body;
-
-	Article(int id, String title, String body) {
-		this.id = id;
-		this.title = title;
-		this.body = body;
 	}
 }
